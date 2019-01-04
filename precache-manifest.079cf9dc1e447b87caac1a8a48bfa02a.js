be13752df8bfda5328814029bb871e9dc76c3bd9
Roger-Wu <gsetcrw@gmail.com>
2019-01-05 00:34:43 +0800

    add price tag

---
+++ precache-manifest.079cf9dc1e447b87caac1a8a48bfa02a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f7a48a0a820bc086ae8a",
+    "url": "./static/js/main.f7a48a0a.chunk.js"
+  },
+  {
+    "revision": "a208ddd93c8974d7a8ce",
+    "url": "./static/js/1.a208ddd9.chunk.js"
+  },
+  {
+    "revision": "f7a48a0a820bc086ae8a",
+    "url": "./static/css/main.b7f981d2.chunk.css"
+  },
+  {
+    "revision": "cda8faf4347947e6336fbbe83da1a7ae",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d821796dbc65490e5b4026b22d626c9e.js"
+  "./precache-manifest.079cf9dc1e447b87caac1a8a48bfa02a.js"
 );
 
 workbox.clientsClaim();
