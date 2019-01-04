7fa82ba6cc135e80b791a36bf1a76a91b3c9430d
Roger-Wu <gsetcrw@gmail.com>
2019-01-04 23:58:13 +0800

    update

---
+++ precache-manifest.908cbc5eadff387ba9c8da745e3bd036.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "d4650cd498376f43cbbf",
+    "url": "./static/js/main.d4650cd4.chunk.js"
+  },
+  {
+    "revision": "746dd8695855d5da4b88",
+    "url": "./static/js/1.746dd869.chunk.js"
+  },
+  {
+    "revision": "d4650cd498376f43cbbf",
+    "url": "./static/css/main.b7f981d2.chunk.css"
+  },
+  {
+    "revision": "2f6f1253f135813dd0f2241550320cfd",
+    "url": "./index.html"
+  }
+];
